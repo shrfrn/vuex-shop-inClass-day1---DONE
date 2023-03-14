c708ecae76140fc6288b6d1980480f246ba1f4fb
shrfrn <shrfrn@gmail.com>
2023-03-14 18:53:29 +0200

    Touchup userDetails route

--- pages/UserDetails.js
+++ pages/UserDetails.js
@@ -1,7 +1,13 @@
 export default {
     template: `
-        <section class="user-details">
-            <pre>{{ user }}</pre>
+        <section class="user-details router-view">
+            <h3>{{ user.fullname }}</h3>
+            <ul v-if="user.orders.length">
+                <li v-for="order in user.orders">
+                    <pre>{{ order }}</pre>
+                </li>
+            </ul>
+            <h4 v-else>No orders yet, <RouterLink to="/shop">make your first one</RouterLink></h4>
         </section>
     `,
     computed: {
